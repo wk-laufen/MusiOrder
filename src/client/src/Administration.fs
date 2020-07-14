67b925ac7ff14675f6f1140af7ee6dc5c2aae258
Johannes Egger <j.egger@posteo.at>
2020-07-14 22:45:29 +0200

    Extract Administration component and rewrite using Elmish

---
+++ src/client/src/Administration.fs
@@ -0,0 +1,252 @@
+module Administration
+
+open Elmish
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.FontAwesome
+open Feliz
+open Feliz.Bulma
+open Feliz.UseDeferred
+open Feliz.UseElmish
+open MusiOrder.Models
+open Thoth.Fetch
+open Thoth.Json
+
+type LoadError =
+    | Forbidden
+    | RequestError of string
+
+type UserList = {
+    AuthKey: AuthKey
+    Users: UserInfo list
+    SelectedUser: string option
+    AddPaymentState: Deferred<unit>
+}
+module UserList =
+    let init authKey users = {
+        AuthKey = authKey
+        Users = users
+        SelectedUser = None
+        AddPaymentState = Deferred.HasNotStartedYet
+    }
+
+type Model =
+    | Hidden
+    | Authenticating
+    | Loading of AuthKey
+    | LoadError of LoadError
+    | Loaded of UserList
+
+type Msg =
+    | Show
+    | Load of AuthKey
+    | LoadResult of Result<UserList, LoadError>
+    | SelectUser of userId: string
+    | AddPayment of userId: string * amount: PositiveInteger
+    | AddPaymentResult of Result<(string * float), exn>
+    | Close
+
+let init = Hidden, Cmd.none
+
+let loadUsers authKey = async {
+    let url = sprintf "/api/users?authKey=%s" (AuthKey.toString authKey |> JS.encodeURIComponent)
+    match! Fetch.``tryGet``(url, caseStrategy = CamelCase) |> Async.AwaitPromise with
+    | Ok (users: UserInfo list) -> return Ok (UserList.init authKey users)
+    | Error (FetchFailed response) when response.Status = 403 -> return Error Forbidden
+    | Error e -> return Error (RequestError (Helper.message e))
+}
+
+let addPayment (payment: Payment) = async {
+    let coders =
+        Extra.empty
+        |> Extra.withCustom AuthKey.encode AuthKey.decoder
+        |> Extra.withCustom PositiveInteger.encode PositiveInteger.decoder
+    let! (totalAmount: float) = Fetch.post("/api/payment", payment, caseStrategy = CamelCase, extra = coders) |> Async.AwaitPromise
+    return (payment.UserId, totalAmount)
+}
+
+let update msg state =
+    match msg with
+    | Show -> Authenticating, Cmd.none
+    | Load authKey -> Loading authKey, Cmd.OfAsync.perform loadUsers authKey LoadResult
+    | LoadResult (Ok userList) -> Loaded userList, Cmd.none
+    | LoadResult (Error e) -> LoadError e, Cmd.none
+    | SelectUser userId ->
+        match state with
+        | Loaded userList -> Loaded { userList with SelectedUser = Some userId }, Cmd.none
+        | _ -> state, Cmd.none
+    | AddPayment (userId, amount) ->
+        match state with
+        | Loaded userList ->
+            let payment = { AuthKey = userList.AuthKey; UserId = userId; Amount = amount }
+            Loaded { userList with AddPaymentState = Deferred.InProgress }, Cmd.OfAsync.either addPayment payment (Ok >> AddPaymentResult) (Error >> AddPaymentResult)
+        | _ -> state, Cmd.none
+    | AddPaymentResult (Ok (userId, totalAmount)) ->
+        match state with
+        | Loaded userList ->
+            let users =
+                userList.Users
+                |> List.map (fun user ->
+                    if user.Id = userId then { user with Balance = totalAmount }
+                    else user
+                )
+            Loaded { userList with Users = users; AddPaymentState = Deferred.Resolved () }, Cmd.none
+        | _ -> state, Cmd.none
+    | AddPaymentResult (Error e) ->
+        match state with
+        | Loaded userList -> Loaded { userList with AddPaymentState = Deferred.Failed e }, Cmd.none
+        | _ -> state, Cmd.none
+    | Close -> Hidden, Cmd.none
+
+let view = React.functionComponent (fun () ->
+    let (state, dispatch) = React.useElmish(init, update, [||])
+    let acceptsAuthKey =
+        match state with
+        | Authenticating
+        | LoadError -> true
+        | _ -> false
+    React.useAuthentication acceptsAuthKey (Load >> dispatch)
+
+    let authForm =
+        match state with
+        | Hidden -> Html.none
+        | Authenticating -> View.authForm "Administration" (fun () -> dispatch Close)
+        | Loading -> View.modal "Administration" (fun () -> dispatch Close) [ View.loadIconBig ] []
+        | LoadError Forbidden ->
+            View.modal "Administration" (fun () -> dispatch Close) [
+                Bulma.container [
+                    color.hasTextDanger
+                    spacing.px2
+                    prop.children [
+                        Fa.i [ Fa.Solid.Ban; Fa.Size Fa.Fa8x ] []
+                        Bulma.title.p [
+                            color.hasTextDanger
+                            prop.children [
+                                Html.text "Schlüssel ist nicht authorisiert."
+                                Html.br []
+                                Html.text "Versuche es nochmal mit einem Administrator-Schlüssel."
+                            ]
+                        ]
+                    ]
+                ]
+            ] []
+        | LoadError RequestError ->
+            View.modal "Administration" (fun () -> dispatch Close) [
+                Bulma.container [
+                    color.hasTextDanger
+                    spacing.px2
+                    prop.children [
+                        Fa.i [ Fa.Solid.Key; Fa.Size Fa.Fa8x ] []
+                        Bulma.title.p [
+                            color.hasTextDanger
+                            prop.children [
+                                Html.text "Fehler bei der Authorisierung."
+                                Html.br []
+                                Html.text "Versuche es nochmal mit deinem Musischlüssel."
+                            ]
+                        ]
+                    ]
+                ]
+            ] []
+        | Loaded userList ->
+            View.modal "Administration" (fun () -> dispatch Close) [
+                Bulma.table [
+                    Html.thead [
+                        Html.tr [
+                            Html.th [ prop.text "Nachname" ]
+                            Html.th [ prop.text "Vorname" ]
+                            Html.th [ prop.text "Letzte Bestellung" ]
+                            Html.th [ prop.text "Aktuelles Guthaben" ]
+                        ]
+                    ]
+                    Html.tbody [
+                        for user in userList.Users ->
+                            let (latestOrderColor, latestOrderTime) =
+                                user.LatestOrderTimestamp
+                                |> Option.map (fun v ->
+                                    let m = JS.moment(v)
+                                    let daysSinceLatestOrder = JS.moment(System.DateTimeOffset.Now)?diff(m, "days")
+                                    let color =
+                                        if daysSinceLatestOrder < 10. then Some color.isSuccess
+                                        elif daysSinceLatestOrder < 30. then Some color.isWarning
+                                        else Some color.isDanger
+                                    color, JS.moment(v)?fromNow()
+                                )
+                                |> Option.defaultValue (None, "-")
+                            Html.tr [
+                                prop.onClick (fun _ -> dispatch (SelectUser user.Id))
+                                if userList.SelectedUser = Some user.Id then tr.isSelected
+                                prop.children [
+                                    Html.td [
+                                        text.hasTextLeft
+                                        prop.style [ style.textTransform.uppercase ]
+                                        prop.text user.LastName
+                                    ]
+                                    Html.td [
+                                        text.hasTextLeft
+                                        prop.text user.FirstName
+                                    ]
+                                    Html.td [
+                                        match latestOrderColor with
+                                        | Some color -> color
+                                        | None -> ()
+                                        prop.text latestOrderTime
+                                    ]
+                                    Html.td [
+                                        View.balanceColor user.Balance
+                                        prop.textf "%.2f€" user.Balance
+                                    ]
+                                ]
+                            ]
+                    ]
+                ]
+            ] [
+                match userList.SelectedUser with
+                | Some selectedUserId ->
+                    Html.span [
+                        spacing.mr2
+                        prop.text "Guthaben aufladen:"
+                    ]
+                    Html.div [
+                        Bulma.buttons [
+                            for i in List.choose PositiveInteger.tryCreate [1; 2; 5; 10; 20] ->
+                                Bulma.button.button [
+                                    match userList.AddPaymentState with
+                                    | Deferred.InProgress ->
+                                        prop.disabled true
+                                    | _ -> ()
+                                    prop.textf "+%d€" (PositiveInteger.value i)
+                                    prop.onClick (fun _ -> dispatch (AddPayment (selectedUserId, i)))
+                                ]
+                        ]
+                    ]
+                    let icon iconProps faProps =
+                        Bulma.icon [
+                            control.isMedium
+                            yield! iconProps
+                            prop.children [
+                                Fa.i [
+                                    Fa.Size Fa.FaLarge
+                                    yield! faProps
+                                ] []
+                            ]
+                        ]
+                    match userList.AddPaymentState with
+                    | Deferred.HasNotStartedYet -> ()
+                    | Deferred.InProgress -> icon [ color.hasTextPrimary ] [ Fa.Solid.Spinner; Fa.Pulse ]
+                    | Deferred.Failed e -> icon [ color.hasTextDanger; prop.title e.Message ] [ Fa.Solid.Times ]
+                    | Deferred.Resolved -> icon [ color.hasTextSuccess ] [ Fa.Solid.Check ]
+                | None -> ()
+            ]
+
+    [
+        Bulma.button.button [
+            prop.onClick (fun _ -> dispatch Show)
+            prop.children [
+                Bulma.icon [ Fa.i [ Fa.Solid.Cogs ] [] ]
+                Html.span [ prop.text "Administration" ]
+            ]
+        ]
+        authForm
+    ]
+)
--- src/client/src/Main.fs
+++ src/client/src/Main.fs
@@ -1,47 +1,14 @@
 module Main
 
 open Browser.Dom
-open Fable.Core
 open Fable.Core.JsInterop
 open Fable.FontAwesome
 open Feliz
 open Feliz.Bulma
-open Feliz.UseDeferred
-open Feliz.UseElmish
 open global.JS
 open MusiOrder.Models
-open Thoth.Fetch
-open Thoth.Json
 
 importAll "../styles/main.scss"
-
-module React =
-    let useAuthentication isActive =
-        let (authKey, setAuthKey) = React.useState(None)
-        React.useEffect (
-            fun () ->
-                if isActive then
-                    let mutable key = ""
-                    let mutable timeoutId = 0.
-                    let rec finishAuthKey () =
-                        setAuthKey (Some (AuthKey key))
-                        key <- ""
-                    and listener (e: Browser.Types.Event) =
-                        window.clearTimeout timeoutId
-                        let newKey = (e :?> Browser.Types.KeyboardEvent).key
-                        if newKey = "Enter" then finishAuthKey ()
-                        else
-                            key <- key + newKey
-                            timeoutId <- window.setTimeout (finishAuthKey, 500)
-                    window.addEventListener("keydown", listener)
-                    React.createDisposable (fun () -> window.removeEventListener("keydown", listener))
-                else
-                    setAuthKey None
-                    React.createDisposable id
-            ,
-            [| box isActive |]
-        )
-        authKey
 
 let nav =
     Bulma.navbar [
@@ -68,220 +35,6 @@ let nav =
         ]
     ]
 
-let showAdministration = React.functionComponent (fun () ->
-    let (isVisible, setVisible) = React.useState(false)
-    let authKey = React.useAuthentication isVisible
-    let startAuthenticate () = setVisible true
-
-    let authorize authKey : Async<Result<AuthKey * UserInfo list, unit>> = async {
-        let url = sprintf "/api/users?authKey=%s" (AuthKey.toString authKey |> JS.encodeURIComponent)
-        match! Fetch.``tryGet``(url, caseStrategy = CamelCase) |> Async.AwaitPromise with
-        | Ok result -> return Ok (authKey, result)
-        | Error (FetchFailed response) when response.Status = 403 -> return Error ()
-        | Error e -> return failwith (Helper.message e)
-    }
-
-    let (authorizationState, setAuthorizationState) = React.useState(Deferred.HasNotStartedYet)
-
-    let startAuthorization = React.useDeferredCallback(authorize, setAuthorizationState)
-
-    React.useEffect(
-        fun () ->
-            match authKey, authorizationState with
-            | Some authKey, Deferred.HasNotStartedYet -> startAuthorization authKey
-            | Some authKey, Deferred.Failed -> startAuthorization authKey
-            | Some authKey, Deferred.Resolved Error -> startAuthorization authKey
-            | _ -> ()
-        ,
-        [| box authKey |]
-    )
-
-    let (selectedUser, setSelectedUser) = React.useState(None)
-    let selectUser = Some >> setSelectedUser
-
-    let addPayment (payment: Payment) = async {
-        let coders =
-            Extra.empty
-            |> Extra.withCustom AuthKey.encode AuthKey.decoder
-            |> Extra.withCustom PositiveInteger.encode PositiveInteger.decoder
-        let! (totalAmount: float) = Fetch.post("/api/payment", payment, caseStrategy = CamelCase, extra = coders) |> Async.AwaitPromise
-        return (payment.UserId, totalAmount)
-    }
-
-    let (paymentState, setPaymentState) = React.useState(Deferred.HasNotStartedYet)
-    let startPayment = React.useDeferredCallback(addPayment, setPaymentState)
-
-    React.useEffect (
-        fun () ->
-            paymentState
-            |> Deferred.iter (fun (userId, totalAmount) ->
-                authorizationState
-                |> Deferred.map (fun result ->
-                    result
-                    |> Result.map (fun (authKey, users) ->
-                        let users' =
-                            users
-                            |> List.map (fun user ->
-                                if user.Id = userId then { user with Balance = totalAmount }
-                                else user
-                            )
-                        (authKey, users')
-                    )
-                )
-                |> setAuthorizationState
-            )
-        , [| box paymentState |]
-    )
-
-    let hideOrderSummary () =
-        setVisible false
-        setAuthorizationState Deferred.HasNotStartedYet
-        setSelectedUser None
-        setPaymentState Deferred.HasNotStartedYet
-
-    let authForm =
-        match authorizationState with
-        | Deferred.HasNotStartedYet -> View.authForm "Administration" hideOrderSummary
-        | Deferred.InProgress -> View.modal "Administration" hideOrderSummary [ View.loadIconBig ] []
-        | Deferred.Failed ->
-            View.modal "Administration" hideOrderSummary [
-                Bulma.container [
-                    color.hasTextDanger
-                    spacing.px2
-                    prop.children [
-                        Fa.i [ Fa.Solid.Key; Fa.Size Fa.Fa8x ] []
-                        Bulma.title.p [
-                            color.hasTextDanger
-                            prop.children [
-                                Html.text "Fehler bei der Authorisierung."
-                                Html.br []
-                                Html.text "Versuche es nochmal mit deinem Musischlüssel."
-                            ]
-                        ]
-                    ]
-                ]
-            ] []
-        | Deferred.Resolved Error ->
-            View.modal "Administration" hideOrderSummary [
-                Bulma.container [
-                    color.hasTextDanger
-                    spacing.px2
-                    prop.children [
-                        Fa.i [ Fa.Solid.Ban; Fa.Size Fa.Fa8x ] []
-                        Bulma.title.p [
-                            color.hasTextDanger
-                            prop.children [
-                                Html.text "Schlüssel ist nicht authorisiert."
-                                Html.br []
-                                Html.text "Versuche es nochmal mit einem Administrator-Schlüssel."
-                            ]
-                        ]
-                    ]
-                ]
-            ] []
-        | Deferred.Resolved (Ok (authKey, users)) ->
-            View.modal "Administration" hideOrderSummary [
-                Bulma.table [
-                    Html.thead [
-                        Html.tr [
-                            Html.th [ prop.text "Nachname" ]
-                            Html.th [ prop.text "Vorname" ]
-                            Html.th [ prop.text "Letzte Bestellung" ]
-                            Html.th [ prop.text "Aktuelles Guthaben" ]
-                        ]
-                    ]
-                    Html.tbody [
-                        for user in users ->
-                            let (latestOrderColor, latestOrderTime) =
-                                user.LatestOrderTimestamp
-                                |> Option.map (fun v ->
-                                    let m = moment(v)
-                                    let daysSinceLatestOrder = moment(System.DateTimeOffset.Now)?diff(m, "days")
-                                    let color =
-                                        if daysSinceLatestOrder < 10. then Some color.isSuccess
-                                        elif daysSinceLatestOrder < 30. then Some color.isWarning
-                                        else Some color.isDanger
-                                    color, moment(v)?fromNow()
-                                )
-                                |> Option.defaultValue (None, "-")
-                            Html.tr [
-                                prop.onClick (fun _ -> selectUser user.Id)
-                                if selectedUser = Some user.Id then tr.isSelected
-                                prop.children [
-                                    Html.td [
-                                        text.hasTextLeft
-                                        prop.style [ style.textTransform.uppercase ]
-                                        prop.text user.LastName
-                                    ]
-                                    Html.td [
-                                        text.hasTextLeft
-                                        prop.text user.FirstName
-                                    ]
-                                    Html.td [
-                                        match latestOrderColor with
-                                        | Some color -> color
-                                        | None -> ()
-                                        prop.text latestOrderTime
-                                    ]
-                                    Html.td [
-                                        View.balanceColor user.Balance
-                                        prop.textf "%.2f€" user.Balance
-                                    ]
-                                ]
-                            ]
-                    ]
-                ]
-            ] [
-                match selectedUser with
-                | Some selectedUserId ->
-                    Html.span [
-                        spacing.mr2
-                        prop.text "Guthaben aufladen:"
-                    ]
-                    Html.div [
-                        Bulma.buttons [
-                            for i in List.choose PositiveInteger.tryCreate [1; 2; 5; 10; 20] ->
-                                Bulma.button.button [
-                                    match paymentState with
-                                    | Deferred.InProgress ->
-                                        prop.disabled true
-                                    | _ -> ()
-                                    prop.textf "+%d€" (PositiveInteger.value i)
-                                    prop.onClick (fun _ -> startPayment { AuthKey = authKey; UserId = selectedUserId; Amount = i })
-                                ]
-                        ]
-                    ]
-                    let icon iconProps faProps =
-                        Bulma.icon [
-                            control.isMedium
-                            yield! iconProps
-                            prop.children [
-                                Fa.i [
-                                    Fa.Size Fa.FaLarge
-                                    yield! faProps
-                                ] []
-                            ]
-                        ]
-                    match paymentState with
-                    | Deferred.HasNotStartedYet -> ()
-                    | Deferred.InProgress -> icon [ color.hasTextPrimary ] [ Fa.Solid.Spinner; Fa.Pulse ]
-                    | Deferred.Failed e -> icon [ color.hasTextDanger; prop.title e.Message ] [ Fa.Solid.Times ]
-                    | Deferred.Resolved -> icon [ color.hasTextSuccess ] [ Fa.Solid.Check ]
-                | None -> ()
-            ]
-
-    [
-        Bulma.button.button [
-            prop.onClick (ignore >> startAuthenticate)
-            prop.children [
-                Bulma.icon [ Fa.i [ Fa.Solid.Cogs ] [] ]
-                Html.span [ prop.text "Administration" ]
-            ]
-        ]
-        if isVisible then authForm
-    ]
-)
-
 let main =
     Html.div [
         prop.className "main"
@@ -290,7 +43,7 @@ let main =
             OrderForm.view
                 {|
                     UserButtons = [ OrderSummary.view () ]
-                    AdminButtons = [ showAdministration () ]
+                    AdminButtons = [ Administration.view () ]
                 |}
         ]
     ]
