e63d755c6dd4a863510fcdab82c8251abd9a5a1c
Johannes Egger <j.egger@posteo.at>
2021-07-04 17:12:12 +0200

    Add container name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -d --restart unless-stopped -p 80:80 -e DB_PATH="/app/data/musiorder.db" -v "$PWD/data:/app/data" musiorder
+docker run --name musiorder -d --restart unless-stopped -p 80:80 -e DB_PATH="/app/data/musiorder.db" -v "$PWD/data:/app/data" musiorder
