8f3cac9933e016be437cb4619f7d84aabc7905e4
Johannes Egger <j.egger@posteo.at>
2021-07-30 14:07:18 +0200

    Add script to build and publish image

---
+++ build-and-publish.ps1
@@ -0,0 +1,5 @@
+$version = "0.1"
+# docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t johannesegger/musiorder:$version -t johannesegger/musiorder:latest --push .
+docker build -t johannesegger/musiorder:$version -t johannesegger/musiorder:latest .
+docker push johannesegger/musiorder:$version
+docker push johannesegger/musiorder:latest
