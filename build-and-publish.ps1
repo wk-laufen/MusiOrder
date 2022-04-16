976dc2297626dc229581d6fd938c1cdd31136d42
Johannes Egger <j.egger@posteo.at>
2022-04-16 07:33:23 +0200

    Update to v0.4

--- build-and-publish.ps1
+++ build-and-publish.ps1
@@ -1,4 +1,4 @@
-$version = "0.3"
+$version = "0.4"
 # docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t johannesegger/musiorder:$version -t johannesegger/musiorder:latest --push .
 docker build -t johannesegger/musiorder:$version -t johannesegger/musiorder:latest .
 docker push johannesegger/musiorder:$version
