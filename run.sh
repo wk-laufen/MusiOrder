1a34048357284fff947dc5d0d1b763d5692ea243
Johannes Egger <j.egger@posteo.at>
2022-04-29 20:47:33 +0200

    Fix bash script variable assignment

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-VERSION = $1
+VERSION=$1
 docker run --name musiorder-$VERSION -d --restart unless-stopped -p 80:80 -e DB_PATH="/app/data/musiorder.db" -v "$PWD/data:/app/data" johannesegger/musiorder:$VERSION
