8948e4bfab184d921fe7e4acde49161d21965c8c
Johannes Egger <j.egger@posteo.at>
2022-04-29 20:46:28 +0200

    Require version when running

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run --name musiorder -d --restart unless-stopped -p 80:80 -e DB_PATH="/app/data/musiorder.db" -v "$PWD/data:/app/data" johannesegger/musiorder
+VERSION = $1
+docker run --name musiorder-$VERSION -d --restart unless-stopped -p 80:80 -e DB_PATH="/app/data/musiorder.db" -v "$PWD/data:/app/data" johannesegger/musiorder:$VERSION
