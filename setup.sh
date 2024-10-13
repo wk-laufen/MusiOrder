fe88da2e40ebb5fb9318d31596e60a3976415738
Johannes Egger <j.egger@posteo.at>
2024-10-13 21:07:21 +0200

    Clean up setup.sh

--- setup.sh
+++ setup.sh
@@ -4,22 +4,16 @@
 #   * Set host name, enable SSH, set username/password, configure WIFI, set locale settings
 # 2. Run `sudo raspi-config` to setup console autologin
 
-sudo apt update
-sudo apt install --no-install-recommends -y xserver-xorg x11-xserver-utils xinit openbox git chromium-browser sqlite3 jq
+SERVER_URL="http://localhost"
 
+echo "== Install docker =="
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker $USER
 rm get-docker.sh
 
-CHROMIUM_CONFIG_FILE="$HOME/.config/chromium/Default/Preferences"
-if [ -f "$CHROMIUM_CONFIG_FILE" ]; then
-    cat "$CHROMIUM_CONFIG_FILE" | jq '.download.prompt_for_download = true | .download.default_directory = "/usb/"' | tee "$CHROMIUM_CONFIG_FILE" > /dev/null
-else
-    mkdir -p "$(dirname $CHROMIUM_CONFIG_FILE)"
-    echo '{"download": { "prompt_for_download": true, "default_directory": "/usb" } }' > "$CHROMIUM_CONFIG_FILE"
-fi
-
+echo "== Setup kiosk mode with Chromium =="
+sudo apt update && sudo apt install --no-install-recommends -y xserver-xorg x11-xserver-utils xinit openbox chromium-browser
 echo "xset -dpms # Turn off display power management system
 xset s noblank # Turn off screen blanking
 xset s off # Turn off screen saver
@@ -32,7 +26,7 @@ sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[
 chromium-browser --noerrdialogs --disable-infobars --kiosk \$KIOSK_URL
 " | sudo tee -a /etc/xdg/openbox/autostart > /dev/null
 
-echo "export KIOSK_URL=http://localhost" | sudo tee -a /etc/xdg/openbox/environment > /dev/null
+echo "export KIOSK_URL=$SERVER_URL" | sudo tee -a /etc/xdg/openbox/environment > /dev/null
 
 echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor" >> ~/.bash_profile
 
@@ -46,10 +40,7 @@ hdmi_cvt 800 480 60 6 0 0 0
 dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900
 display_rotate=0" | sudo tee -a /boot/config.txt > /dev/null
 
-# Auto-mount USB drives
-sudo mkdir /usb && sudo chown pi:pi /usb
-git clone https://github.com/antonilol/simple-usb-automount
-pushd ./simple-usb-automount
-sudo ./install
-sed 's/mntopts="/&uid=pi,gid=pi,/' /usr/local/bin/simple-usb-automount | sudo tee /usr/local/bin/simple-usb-automount > /dev/null
-popd
+echo "== Setup NFC reader =="
+sudo apt update && sudo apt install -y pcscd pcsc-tools
+echo -e 'install nfc /bin/false\ninstall pn533 /bin/false' | sudo tee /etc/modprobe.d/blacklist.conf
+sudo reboot now
