a2554c929e23c18ca1b0fb72589d67acefad61ac
Johannes Egger <j.egger@posteo.at>
2024-10-13 20:35:25 +0200

    Clean up

--- build-and-publish.ps1
+++
@@ -1,5 +0,0 @@
-$version = "0.8"
-# docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t johannesegger/musiorder:$version -t johannesegger/musiorder:latest --push .
-docker build -t johannesegger/musiorder:$version -t johannesegger/musiorder:latest .
-docker push johannesegger/musiorder:$version
-docker push johannesegger/musiorder:latest
