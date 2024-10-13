79d99cee400a078488c32e08265122d7bb59a7fa
Johannes Egger <j.egger@posteo.at>
2024-10-13 21:05:54 +0200

    Remove run.sh in favor of docker compose

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-VERSION=$1
-docker run --name musiorder-$VERSION -d --restart unless-stopped -p 80:80 -e DB_PATH="/app/data/musiorder.db" -v "$PWD/data:/app/data" johannesegger/musiorder:$VERSION
