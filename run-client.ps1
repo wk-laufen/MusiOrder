b2912c2e73e09e6c74a209f48a838e7cf56923ea
Johannes Egger <j.egger@posteo.at>
2024-10-23 16:42:18 +0200

    Fix run-client.ps1 for uninitialized projects

--- run-client.ps1
+++ run-client.ps1
@@ -1,2 +1,6 @@
+dotnet tool restore
+Push-Location .\src\client
+yarn install --frozen-lockfile
+Pop-Location
 $env:NODE_OPTIONS = "--openssl-legacy-provider"
 dotnet fable watch .\src\client\src --run yarn --cwd .\src\client start
