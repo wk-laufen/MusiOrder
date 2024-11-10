7db157ae969b46f080fa2b0de3a706da44d577b7
Johannes Egger <j.egger@posteo.at>
2024-11-10 19:56:55 +0100

    Add run.ps1 to start Server, Client and NFCReader

---
+++ run.ps1
@@ -0,0 +1,4 @@
+wt `
+    new-tab --startingDirectory . powershell .\run-server.ps1`; `
+    new-tab --startingDirectory . powershell .\run-nfc-reader.ps1`; `
+    new-tab --startingDirectory . powershell .\run-client.ps1
