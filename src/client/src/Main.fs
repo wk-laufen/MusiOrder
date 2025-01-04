17b7430dfbf70edcc5488d1d1d6138032655809b
Johannes Egger <j.egger@posteo.at>
2025-01-04 08:24:22 +0100

    Replace navbar styling with tailwind

--- src/client/src/Main.fs
+++ src/client/src/Main.fs
@@ -5,7 +5,6 @@ open Elmish
 open Fable.Core.JsInterop
 open Fable.FontAwesome
 open Feliz
-open Feliz.Bulma
 open Feliz.Router
 open Feliz.UseElmish
 open global.JS
@@ -26,28 +25,18 @@ let update msg state =
     match msg with
     | UrlChanged segments -> { state with CurrentUrl = segments }, Cmd.none
 
-let nav bgColor (title: string) =
-    Bulma.navbar [
-        bgColor
-        prop.className "navigation"
+let nav classes (title: string) =
+    Html.div [
+        prop.className $"%s{classes}"
         prop.children [
-            Bulma.navbarBrand.div [
-                prop.style [
-                    style.alignItems.baseline
-                ]
+            Html.h1 [
+                prop.className "px-4 py-2 text-5xl"
                 prop.children [
-                    Bulma.navbarItem.div [
-                        Html.h1 [
-                            prop.className "text-5xl"
-                            prop.children [
-                                Html.span [ prop.text title ]
-                                Html.a [
-                                    prop.className "cursor-pointer"
-                                    prop.href "/"
-                                    prop.text "🍻"
-                                ]
-                            ]
-                        ]
+                    Html.span [ prop.text title ]
+                    Html.a [
+                        prop.className "cursor-pointer"
+                        prop.href "/"
+                        prop.text "🍻"
                     ]
                 ]
             ]
@@ -78,18 +67,18 @@ let Main () =
 
         router.children [
             Html.div [
-                prop.className "main"
+                prop.className "flex flex-col h-full"
                 prop.children [
                     match state.CurrentUrl with
                     | Administration.route :: subUrl ->
-                        nav color.hasBackgroundDanger "MusiOrder - Administration"
+                        nav "bg-musi-red" "MusiOrder - Administration"
                         let activeTab =
                             Administration.allTabs
                             |> List.tryFind (fun v -> subUrl = [ Administration.Tab.toRoute v ])
                             |> Option.defaultValue (List.head Administration.allTabs)
                         Administration.Administration activeTab
                     | _ ->
-                        nav color.hasBackgroundPrimary "MusiOrder"
+                        nav "bg-musi-gold" "MusiOrder"
                         OrderForm.OrderForm
                             [ OrderSummary.OrderSummary () ]
                             [ adminPageButton ]
